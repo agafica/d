48c775459e6ef9be16fc06f377c74e80a863cc9f
Albina <sagafio@mail.ru>
2023-07-06 20:30:34 +0300

    Task1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*Task 1 Напишите программу, которая на вход принимает два числа и выдаёт большее число.*/
+
+
+Console.Write("Input a first number1 ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a first number2 ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if ( num1 > num2 )Console.WriteLine($"maximum num1");
+else Console.WriteLine("maximum num" + num2);
+
+
+
+
+
